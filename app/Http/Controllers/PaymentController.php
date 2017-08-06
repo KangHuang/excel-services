4bd726c0d344f77e7682903da0db152671c8241c
KangHuang <huangkang2016@gmail.com>
2017-08-06 22:41:36 +0100

    try ipn2

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,311 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use PayPal\Rest\ApiContext;
+use PayPal\Auth\OAuthTokenCredential;
+use PayPal\Api\Amount;
+use PayPal\Api\Payee;
+use PayPal\Api\Payer;
+use PayPal\Api\Payment;
+use PayPal\Api\RedirectUrls;
+use PayPal\Api\Transaction;
+use PayPal\Api\PaymentExecution;
+
+class PaymentController extends Controller {
+
+    private $apiContext;
+
+    public function __construct() {
+        $this->apiContext = new ApiContext(
+                new OAuthTokenCredential(
+                'AR-jrkIvtn1GgorJm8iFYigICRAdRvuABkso2bpkJVPJuSDtZtsz1Hh4Ag-MuduM5Yizd1Aei-V9p3M9', // ClientID
+                'EPqV4chEaJrNIdtf-Cupfm5CoTLYQH1Yktu3h_PGjOfF1qdjfikefK-H9pNO889xrCW_WmFe1ggtAWOT'      // ClientSecret
+                )
+        );
+    }
+
+    /**
+     * Make payment.
+     *
+     * @return 
+     */
+    public function createPayment() {
+        // ### Payer
+// A resource representing a Payer that funds a payment
+// For paypal account payments, set payment method
+// to 'paypal'.
+        $payer = new Payer();
+        $payer->setPaymentMethod("paypal");
+// ### Amount
+// Lets you specify a payment amount.
+// You can also specify additional details
+// such as shipping, tax.
+        $amount = new Amount();
+        $amount->setCurrency("GBP")
+                ->setTotal(66);
+// ### Payee
+// Specify a payee with that user's email or merchant id
+// Merchant Id can be found at https://www.paypal.com/businessprofile/settings/
+        $payee = new Payee();
+        $payee->setEmail("huangkang2016-payee1@gmail.com");
+// ### Transaction
+// A transaction defines the contract of a
+// payment - what is the payment for and who
+// is fulfilling it.
+        $transaction = new Transaction();
+        $transaction->setAmount($amount)
+                ->setDescription("Service Payment")
+                ->setPayee($payee)
+                ->setInvoiceNumber(uniqid());
+// ### Redirect urls
+// Set the urls that the buyer must be redirected to after
+// payment approval/ cancellation.
+        $baseUrl = $this->getBaseUrl();
+        $redirectUrls = new RedirectUrls();
+        $redirectUrls->setReturnUrl("$baseUrl./service/executePayment?success=true")
+                ->setCancelUrl("$baseUrl./service/executePayment?success=false");
+// ### Payment
+// A Payment Resource; create one using
+// the above types and intent set to 'sale'
+        $payment = new Payment();
+        $payment->setIntent("sale")
+                ->setPayer($payer)
+                ->setRedirectUrls($redirectUrls)
+                ->setTransactions(array($transaction));
+// For Sample Purposes Only.
+        $request = clone $payment;
+// ### Create Payment
+// Create a payment by calling the 'create' method
+// passing it a valid apiContext.
+// (See bootstrap.php for more on `ApiContext`)
+// The return object contains the state and the
+// url to which the buyer must be redirected to
+// for payment approval
+        try {
+            $payment->create($this->apiContext);
+        } catch (Exception $ex) {
+            // NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
+            exit(1);
+        }
+// ### Get redirect url
+// The API response provides the url that you must redirect
+// the buyer to. Retrieve the url from the $payment->getApprovalLink()
+// method
+        $approvalUrl = $payment->getApprovalLink();
+        return view('front.service.payment', compact('approvalUrl'));
+    }
+
+    /*
+      function execute payment
+     * @return payment
+     *      */
+
+    public function executePayment() {
+        // ### Approval Status
+// Determine if the user approved the payment or not
+        if (isset($_GET['success']) && $_GET['success'] == 'true') {
+            // Get the payment Object by passing paymentId
+            // payment id was previously stored in session in
+            // CreatePaymentUsingPayPal.php
+            $paymentId = $_GET['paymentId'];
+            $payment = Payment::get($paymentId, $this->apiContext);
+            // ### Payment Execute
+            // PaymentExecution object includes information necessary
+            // to execute a PayPal account payment.
+            // The payer_id is added to the request query parameters
+            // when the user is redirected from paypal back to your site
+            $execution = new PaymentExecution();
+            $execution->setPayerId($_GET['PayerID']);
+
+            try {
+                // Execute the payment
+                // (See bootstrap.php for more on `ApiContext`)
+                $result = $payment->execute($execution, $this->apiContext);
+                try {
+                    $payment = Payment::get($paymentId, $this->apiContext);
+                } catch (Exception $ex) {
+                    exit(1);
+                }
+            } catch (Exception $ex) {
+                exit(1);
+            }
+            return $result;
+        } else {
+            exit;
+        }
+    }
+
+    /**
+     * ### getBaseUrl function
+     * // utility function that returns base url for
+     * // determining return/cancel urls
+     *
+     * @return string
+     */
+    public function getBaseUrl() {
+        if (PHP_SAPI == 'cli') {
+            $trace = debug_backtrace();
+            $relativePath = substr(dirname($trace[0]['file']), strlen(dirname(dirname(__FILE__))));
+            echo "Warning: This sample may require a server to handle return URL. Cannot execute in command line. Defaulting URL to http://localhost$relativePath \n";
+            return "http://localhost" . $relativePath;
+        }
+        $protocol = 'http';
+        if ($_SERVER['SERVER_PORT'] == 443 || (!empty($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) == 'on')) {
+            $protocol .= 's';
+        }
+        $host = $_SERVER['HTTP_HOST'];
+        $request = $_SERVER['PHP_SELF'];
+        return dirname($protocol . '://' . $host . $request);
+    }
+
+    /**
+     * ### listen IPN from paypal
+     *
+     *
+     * @return mix
+     */
+    public function ipnListener() {
+
+        $ipn = new PaypalIPN();
+// Use the sandbox endpoint during testing.
+        $ipn->useSandbox();
+        $verified = $ipn->verifyIPN();
+        if ($verified) {
+            if($_POST['txn_type']=='express_checkout'){
+                \Illuminate\Support\Facades\Log::info('ipnsuccess');
+            }
+        }
+// Reply with an empty 200 response to indicate to paypal the IPN was received correctly.
+        header("HTTP/1.1 200 OK");
+    }
+
+}
+
+
+class PaypalIPN
+{
+    /**
+     * @var bool $use_sandbox     Indicates if the sandbox endpoint is used.
+     */
+    private $use_sandbox = false;
+    /**
+     * @var bool $use_local_certs Indicates if the local certificates are used.
+     */
+    private $use_local_certs = true;
+    /** Production Postback URL */
+    const VERIFY_URI = 'https://ipnpb.paypal.com/cgi-bin/webscr';
+    /** Sandbox Postback URL */
+    const SANDBOX_VERIFY_URI = 'https://ipnpb.sandbox.paypal.com/cgi-bin/webscr';
+    /** Response from PayPal indicating validation was successful */
+    const VALID = 'VERIFIED';
+    /** Response from PayPal indicating validation failed */
+    const INVALID = 'INVALID';
+    /**
+     * Sets the IPN verification to sandbox mode (for use when testing,
+     * should not be enabled in production).
+     * @return void
+     */
+    public function useSandbox()
+    {
+        $this->use_sandbox = true;
+    }
+    /**
+     * Sets curl to use php curl's built in certs (may be required in some
+     * environments).
+     * @return void
+     */
+    public function usePHPCerts()
+    {
+        $this->use_local_certs = false;
+    }
+    /**
+     * Determine endpoint to post the verification data to.
+     * @return string
+     */
+    public function getPaypalUri()
+    {
+        if ($this->use_sandbox) {
+            return self::SANDBOX_VERIFY_URI;
+        } else {
+            return self::VERIFY_URI;
+        }
+    }
+    /**
+     * Verification Function
+     * Sends the incoming post data back to PayPal using the cURL library.
+     *
+     * @return bool
+     * @throws Exception
+     */
+    public function verifyIPN()
+    {
+        if ( ! count($_POST)) {
+            throw new Exception("Missing POST Data");
+        }
+        $raw_post_data = file_get_contents('php://input');
+        $raw_post_array = explode('&', $raw_post_data);
+        $myPost = array();
+        foreach ($raw_post_array as $keyval) {
+            $keyval = explode('=', $keyval);
+            if (count($keyval) == 2) {
+                // Since we do not want the plus in the datetime string to be encoded to a space, we manually encode it.
+                if ($keyval[0] === 'payment_date') {
+                    if (substr_count($keyval[1], '+') === 1) {
+                        $keyval[1] = str_replace('+', '%2B', $keyval[1]);
+                    }
+                }
+                $myPost[$keyval[0]] = urldecode($keyval[1]);
+            }
+        }
+        // Build the body of the verification post request, adding the _notify-validate command.
+        $req = 'cmd=_notify-validate';
+        $get_magic_quotes_exists = false;
+        if (function_exists('get_magic_quotes_gpc')) {
+            $get_magic_quotes_exists = true;
+        }
+        foreach ($myPost as $key => $value) {
+            if ($get_magic_quotes_exists == true && get_magic_quotes_gpc() == 1) {
+                $value = urlencode(stripslashes($value));
+            } else {
+                $value = urlencode($value);
+            }
+            $req .= "&$key=$value";
+        }
+        // Post the data back to PayPal, using curl. Throw exceptions if errors occur.
+        $ch = curl_init($this->getPaypalUri());
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $req);
+        curl_setopt($ch, CURLOPT_SSLVERSION, 6);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+        // This is often required if the server is missing a global cert bundle, or is using an outdated one.
+        if ($this->use_local_certs) {
+            curl_setopt($ch, CURLOPT_CAINFO, __DIR__ . "/cert/cacert.pem");
+        }
+        curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Connection: Close'));
+        $res = curl_exec($ch);
+        if ( ! ($res)) {
+            $errno = curl_errno($ch);
+            $errstr = curl_error($ch);
+            curl_close($ch);
+            throw new Exception("cURL error: [$errno] $errstr");
+        }
+        $info = curl_getinfo($ch);
+        $http_code = $info['http_code'];
+        if ($http_code != 200) {
+            throw new Exception("PayPal responded with http code $http_code");
+        }
+        curl_close($ch);
+        // Check if PayPal verifies the IPN data, and if so, return true.
+        if ($res == self::VALID) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
